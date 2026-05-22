2d4746d84c107b628f87b474be8f9154e578a8e2
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2026-05-22 06:44:57 +0000

    Enable Firefox and WebKit projects in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
   },
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
-    // { name: "firefox", use: { ...devices["Desktop Firefox"] } },
-    // { name: "webkit", use: { ...devices["Desktop Safari"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
   ],
   webServer: [
     {
